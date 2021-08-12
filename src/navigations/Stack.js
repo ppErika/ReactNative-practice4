5f54c9f8089d3824c41448865fe6a46572c3d59b
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-12 12:09:33 +0900

    cardStyle이 안먹는 오류가 있으나 왜안되는지 모르겠음 ㅜㅜ

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -6,10 +6,20 @@ const Stack = createNativeStackNavigator();
 
 const StackNav = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        // 지금 이 코드가 안먹는데 이유를 모르겠네 정말
+        cardStyle: {backgroundColor: '#ffffff'},
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        // 마찬가지로 여기서 옵션을 줘도 안먹는데 이유를 정말 모르겠다
+        // options={{cardStyle: {backgroundColor: '#ffffff'}}}
+      />
       <Stack.Screen name="List" component={List} />
       <Stack.Screen name="Chat" component={Chat} />
-      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
 };
