e5d923258ec91db95dc3c5231737699e6a7735c2
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-12 12:17:18 +0900

    홈화면에서는 헤더보이지 않기

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -15,8 +15,7 @@ const StackNav = () => {
       <Stack.Screen
         name="Home"
         component={Home}
-        // 마찬가지로 여기서 옵션을 줘도 안먹는데 이유를 정말 모르겠다
-        // options={{cardStyle: {backgroundColor: '#ffffff'}}}
+        options={{headerShown: false}}
       />
       <Stack.Screen name="List" component={List} />
       <Stack.Screen name="Chat" component={Chat} />
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Button from '../components/Button';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const Container = styled.View`
   align-items: center;
+  // 여기서 패딩값을 줘서 노치디자인 문제 해결
 `;
 
 const StyledText = styled.Text`
@@ -12,6 +14,10 @@ const StyledText = styled.Text`
 `;
 
 const Home = ({navigation}) => {
+  const insets = useSafeAreaInsets();
+  // 각 값만큼을 띄워야 화면이 가려지는 부분이 없다 (노치디자인 문제)
+  // console.log(insets);
+  // 출력 결과: {"bottom": 0, "left": 0, "right": 0, "top": 0}
   return (
     <Container>
       <StyledText>Home</StyledText>
